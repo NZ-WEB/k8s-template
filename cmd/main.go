cdb770e4092886075fb6e1efdba9a08b73c647e1
nzweb <nzweb@vk.com>
2022-12-25 22:20:18 +0300

    feat: add docker

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"k8s-template/internal/handlers"
 	"k8s-template/pkg/version"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 // How to try it: PORT=8000 go run main.go
@@ -21,6 +24,28 @@ func main() {
 	}
 
 	r := handlers.Router(version.BuildTime, version.Commit, version.Release)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: r,
+	}
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
 	log.Print("The service is ready to listen and serve.")
-	log.Fatal(http.ListenAndServe(":"+port, r))
+
+	killSignal := <-interrupt
+	switch killSignal {
+	case os.Interrupt:
+		log.Print("Got SIGINT...")
+	case syscall.SIGTERM:
+		log.Print("Got SIGTERM...")
+	}
+
+	log.Print("The service is shutting down...")
+	srv.Shutdown(context.Background())
+	log.Print("Done")
 }
